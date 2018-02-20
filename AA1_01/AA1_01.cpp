dc9f0df2c0cb8257f97201d407a02e5fc661acee
juan lojo callon <juan230599@hotmail.com>
2018-02-20 11:58:12 +0100

    más ordenado gracias a los pragma

--- AA1_01/AA1_01.cpp
+++ AA1_01/AA1_01.cpp
@@ -11,14 +11,19 @@ enum EnemyType {zombie, vampire, ghost, witch};
 
 //2)
 
+#pragma region "//2 version 1"
 struct Enemy
 {
 	EnemyType type;
 	int health;
 	string name;
 };
+#pragma endregion
+
 
 //3)
+
+#pragma region "// 3 version 1"
 
 bool equalEnnemies(Enemy enemi1, Enemy enemi2)
 {
@@ -32,8 +37,11 @@ bool equalEnnemies(Enemy enemi1, Enemy enemi2)
 	}*/
 	return (enemi1.type == enemi2.type && enemi1.name == enemi2.name);
 }
+#pragma endregion
 
 //4)
+
+#pragma region "//4 version 1"
 
 Enemy createRandomEnemy()
 {
@@ -44,26 +52,31 @@ Enemy createRandomEnemy()
 
 	return enemy1;
 }
+#pragma endregion
 
 //5)
 
+#pragma region "//5 version 1"
+
 string getEnemyTypeString(EnemyType enemy1)
 {
-	switch (EnemyType)
+	switch (enemy1)
 	{
-	case zombie:
+	case EnemyType::zombie:
 		return "zombie";
 		break;
-	case vampire:
+	case EnemyType::vampire:
 		return "vampire";
 		break;
-	case ghost:
+	case EnemyType::ghost:
 		return "ghost";
 		break;
-	case witch:
+	case EnemyType::witch:
 		return "witch";
 		break;
 	default:
+		return "";
 		break;
 	}
 }
+#pragma endregion
